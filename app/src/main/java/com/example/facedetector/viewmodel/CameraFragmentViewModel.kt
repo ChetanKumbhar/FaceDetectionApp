2a78f23e51cfb8c711b56a6c9bb36dc7430d0ed6
gayatri kumbhar <kumbhargayatriavinash@gmail.com>
2022-09-08 18:08:07 +0900

    added view model  for camera fragment

--- app/src/main/java/com/example/facedetector/viewmodel/CameraFragmentViewModel.kt
+++ app/src/main/java/com/example/facedetector/viewmodel/CameraFragmentViewModel.kt
@@ -18,21 +18,79 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CameraFragmentViewModel @Inject constructor(
-    private val cameraHelper: CameraHelper
+
 ) : ViewModel() {
 
     private val TAG = "CameraFragmentViewModel"
-    var imageSavedEvent = MutableLiveData<Boolean>()
-    //private lateinit var outputDirectory: File
 
     companion object {
         const val FILENAME = "yyyy-MM-dd"   //"yyyy-MM-dd-HH-mm-ss-SSS"
     }
 
-    /** Initialize CameraX, and prepare to bind the camera use cases  */
-    /*fun setUpCamera(context: Context, previewView: PreviewView, fragment: Fragment) {
-        cameraHelper.setUpCamera(context, previewView, fragment)
-    }*/
+    var lensFacing: Int = CameraSelector.LENS_FACING_FRONT
+     private var preview: Preview? = null
+     var imageCapture: ImageCapture? = null
+     private var camera: Camera? = null
+     private var cameraProvider: ProcessCameraProvider? = null
+     private lateinit var outputDirectory: File
+     private var eventListener : EventListener? = null
+
+     fun setUpCamera(context: Context, previewView : PreviewView, fragment: Fragment) {
+
+             this.eventListener = fragment as EventListener
+
+         val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+         cameraProviderFuture.addListener(Runnable {
+             // CameraProvider
+             cameraProvider = cameraProviderFuture.get()
+
+             // Build and bind the camera use cases
+             bindCameraUseCases(fragment, previewView)
+         }, ContextCompat.getMainExecutor(context))
+     }
+
+     //Declare and bind preview, capture and analysis use cases
+    private fun bindCameraUseCases(context: Fragment, previewView: PreviewView) {
+        // CameraProvider
+        val cameraProvider = cameraProvider
+            ?: throw IllegalStateException("Camera initialization failed.")
+        if(!cameraProvider.hasCamera(CameraSelector.DEFAULT_FRONT_CAMERA)){
+            lensFacing = CameraSelector.LENS_FACING_BACK
+        }
+
+        // CameraSelector
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+        // Preview
+        preview = Preview.Builder().build()
+
+        // ImageCapture
+        imageCapture = ImageCapture.Builder()
+            .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+            .build()
+
+        // Must unbind the use-cases before rebinding them
+        cameraProvider.unbindAll()
+
+        try {
+            // A variable number of use-cases can be passed here -
+            // camera provides access to CameraControl & CameraInfo
+            camera = cameraProvider.bindToLifecycle(
+                context, cameraSelector, preview, imageCapture)
+
+            // Attach the viewfinder's surface provider to preview use case
+            preview?.setSurfaceProvider(previewView.surfaceProvider)
+        } catch (exc: Exception) {
+            Log.e(TAG, "Use case binding failed", exc)
+        }
+    }
+
+    fun getImageMetaData(): ImageCapture.Metadata {
+        return ImageCapture.Metadata().apply {
+            // Mirror image when using the front camera
+            isReversedHorizontal = lensFacing == CameraSelector.LENS_FACING_FRONT
+        }
+    }
 
     fun takePhoto(context: Context) {// Get a stable reference of the modifiable image capture use case
         val imageCapture = imageCapture ?: return
@@ -41,7 +99,9 @@ class CameraFragmentViewModel @Inject constructor(
             outputDirectory = StorageHelper.getOutputDirectory(context)
 
             // Create output file to hold the image
-            val photoFile = StorageHelper.createFile(outputDirectory, FILENAME)
+            val photoFile = StorageHelper.createFile(outputDirectory,
+                CameraFragmentViewModel.FILENAME
+            )
 
             // Create output options object which contains file + metadata
             val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile)
@@ -63,78 +123,14 @@ class CameraFragmentViewModel @Inject constructor(
                         val msg = "Photo capture succeeded: ${output.savedUri}"
                         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
                         Log.d(TAG, msg)
-                        clearCamera()
-                        imageSavedEvent.postValue(true)
+                        eventListener?.onPhotoSaved(path = output.savedUri.toString())
                     }
                 }
             )
         }
     }
 
-    fun clearCamera(){
-        cameraProvider?.unbindAll()
-        cameraProvider == null
-        imageCapture == null
+    interface EventListener{
+        fun onPhotoSaved(path : String)
     }
-
-    var lensFacing: Int = CameraSelector.LENS_FACING_FRONT
-    private var preview: Preview? = null
-    var imageCapture: ImageCapture? = null
-    private var camera: Camera? = null
-    private var cameraProvider: ProcessCameraProvider? = null
-    private lateinit var outputDirectory: File
-
-    fun setUpCamera(context: Context, previewView : PreviewView, fragment: Fragment) {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
-        cameraProviderFuture.addListener(Runnable {
-
-            // CameraProvider
-            cameraProvider = cameraProviderFuture.get()
-
-            // Build and bind the camera use cases
-            bindCameraUseCases(fragment, previewView)
-        }, ContextCompat.getMainExecutor(context))
-    }
-
-    /** Declare and bind preview, capture and analysis use cases */
-    private fun bindCameraUseCases(fragment: Fragment, previewView: PreviewView) {
-        // CameraProvider
-        val cameraProvider = cameraProvider
-            ?: throw IllegalStateException("Camera initialization failed.")
-
-        // CameraSelector
-        val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
-
-        // Preview
-        preview = Preview.Builder().build()
-
-        // ImageCapture
-        imageCapture = ImageCapture.Builder()
-            .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-            .build()
-
-        // Must unbind the use-cases before rebinding them
-        cameraProvider.unbindAll()
-
-        try {
-            // A variable number of use-cases can be passed here -
-            // camera provides access to CameraControl & CameraInfo
-            camera = cameraProvider.bindToLifecycle(
-                fragment, cameraSelector, preview, imageCapture)
-
-            // Attach the viewfinder's surface provider to preview use case
-            preview?.setSurfaceProvider(previewView.surfaceProvider)
-        } catch (exc: Exception) {
-            Log.e(TAG, "Use case binding failed", exc)
-        }
-    }
-
-    fun getImageMetaData(): ImageCapture.Metadata {
-        return ImageCapture.Metadata().apply {
-            // Mirror image when using the front camera
-            isReversedHorizontal = lensFacing == CameraSelector.LENS_FACING_FRONT
-        }
-    }
-
-
 }
